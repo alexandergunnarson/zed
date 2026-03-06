f49886298a552625b94994661e3f55201f45d883
Alex Gunnarson <alexandergunnarson@gmail.com>
2026-03-06 15:22:56 -0700

    Revert "fix: disable grep tool temporarily due to hang"
    This reverts commit 1c41a2b1e1566cc9537101ca73ee479c4b9ae283.

--- crates/agent/src/thread.rs
+++ crates/agent/src/thread.rs
@@ -1457,7 +1457,7 @@ impl Thread {
         ));
         self.add_tool(FetchTool::new(self.project.read(cx).client().http_client()));
         self.add_tool(FindPathTool::new(self.project.clone()));
-        // self.add_tool(GrepTool::new(self.project.clone()));
+        self.add_tool(GrepTool::new(self.project.clone()));
         self.add_tool(ListDirectoryTool::new(self.project.clone()));
         self.add_tool(MovePathTool::new(self.project.clone()));
         self.add_tool(NowTool);
